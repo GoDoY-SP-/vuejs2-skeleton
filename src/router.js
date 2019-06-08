e981f24299e01c3fd076e546c28f7c45c24906e6
Danilo GoDoY-SP- <danilo.doring@gmail.com>
2019-06-08 13:11:49 -0300

    Ajustes de arquiteturais e de roteamento

--- src/router.js
+++ src/router.js
@@ -1,23 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
 
-Vue.use(Router)
+Vue.use(Router);
+
+// Carregar view dinamicamente
+function loadView(view) {
+    // route level code-splitting
+    // this generates a separate chunk (view-${view}.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    return () => import(/* webpackChunkName: "view-[request]" */ `@/views/${view}.vue`)
+}
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: loadView('Home'),
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: loadView('About')
+        }
+    ]
 })
